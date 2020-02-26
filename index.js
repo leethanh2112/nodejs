f07202ab7298816a2b0e2e3608ed947026af6a7b
root <leethanh21121986@gmail.com>
2020-02-26 08:32:58 +0700

    nodejs v1.1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello Girl!! I love you so much !!')
+  res.send('Hello Girl!!!')
 })
 
 //Launch listening server on port 8081
